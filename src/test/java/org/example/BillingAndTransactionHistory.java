b89f954bb6bb7943af9237e296886070c12346be
Jonathan <wi24b058@technikum-wien.at>
2026-01-13 13:28:59 +0100

    final touch

---
+++ src/test/java/org/example/BillingAndTransactionHistory.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.datatable.DataTable;
+
+public class BillingAndTransactionHistory {
+    @And("the transaction history contains the following sessions:")
+    public void theTransactionHistoryContainsTheFollowingSessions() {
+
+    }
+
+    @Given("I have completed charging sessions in my account")
+    public void iHaveCompletedChargingSessionsInMyAccount() {
+    }
+
+    @When("I access the transaction history")
+    public void iAccessTheTransactionHistory() {
+    }
+
+    @Then("I should see a chronological list of all my charging sessions")
+    public void iShouldSeeAChronologicalListOfAllMyChargingSessions() {
+    }
+
+    @And("each session should show the following details:")
+    public void eachSessionShouldShowTheFollowingDetails() {
+    }
+
+    @Given("I want to check my current charges for the period {string}")
+    public void iWantToCheckMyCurrentChargesForThePeriod(String arg0) {
+    }
+
+    @And("I have the following charging sessions:")
+    public void iHaveTheFollowingChargingSessions() {
+    }
+
+    @When("I view the billing overview")
+    public void iViewTheBillingOverview() {
+    }
+
+    @Then("I should see the following invoice summary:")
+    public void iShouldSeeTheFollowingInvoiceSummary() {
+    }
+
+    @Given("I am logged into my customer account")
+    public void iAmLoggedIntoMyCustomerAccount() {
+    }
+
+    @And("my account has the following financial status:")
+    public void myAccountHasTheFollowingFinancialStatus() {
+    }
+
+    @When("I check my financial information")
+    public void iCheckMyFinancialInformation() {
+    }
+
+    @Then("I should see my current account balance: {double} EUR")
+    public void iShouldSeeMyCurrentAccountBalanceEUR(int arg0, int arg1) {
+    }
+
+    @And("I should see my available credit: {double} EUR")
+    public void iShouldSeeMyAvailableCreditEUR(int arg0, int arg1) {
+    }
+
+    @And("I should understand if I need to add more funds")
+    public void iShouldUnderstandIfINeedToAddMoreFunds() {
+    }
+
+    @And("I should see the following status indicators:")
+    public void iShouldSeeTheFollowingStatusIndicators() {
+    }
+}
